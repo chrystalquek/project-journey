77f116ebd3229b954341d9214fb9920bea704462
chrystalquek <chrystalquek@yahoo.com.sg>
2020-09-21 00:49:05 +0800

    Add flag to disable authentication

--- server/src/helpers/auth.ts
+++ server/src/helpers/auth.ts
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import { withJWTAuthMiddleware } from "express-kun";
+import config from "../config";
 
 export const accessTokenSecret = 'youraccesstokensecret'; // change? also not v sure where to put this
 
 // helper method to create protected router
 // used in routes directories (see e.g. in routes/user.ts)
 export const createProtectedRouter = (router: Router) => {
-    return withJWTAuthMiddleware(router, accessTokenSecret);
+    return config.disableAuthentication
+        ? router
+        : withJWTAuthMiddleware(router, accessTokenSecret);
 };
