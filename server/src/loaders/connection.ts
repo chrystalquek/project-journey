52cd282137f74a7fb4ec3093d27d6093c3c65f5e
Matthew Alexander <alexandermatthew12@gmail.com>
2020-09-23 19:55:49 +0800

    Enforce airbnb eslint type

--- server/src/loaders/connection.ts
+++ server/src/loaders/connection.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-const mongoDbConnectionString =
-    "mongodb+srv://user:user@cluster0.8ap9j.gcp.mongodb.net/user?retryWrites=true&w=majority";
+import mongoose from 'mongoose';
+
+const mongoDbConnectionString = 'mongodb+srv://user:user@cluster0.8ap9j.gcp.mongodb.net/user?retryWrites=true&w=majority';
 mongoose.connect(mongoDbConnectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connectione error"));
-db.once("open", () => {
-    console.log("connected!");
+db.on('error', console.error.bind(console, 'MongoDB connectione error'));
+db.once('open', () => {
+  console.log('connected!');
 });
 
 export default db;
