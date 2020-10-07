9b21fdfca41fd44f2664b3a3cf1e04995b0c1bf6
Akhil Vuputuri <e0309566@u.nus.edu>
2020-10-08 00:26:05 +0800

    Test trigger with this text change

--- server/src/routes/test.ts
+++ server/src/routes/test.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send("Test"));
+router.get('/', (req, res) => res.send("Testing trigger"));
 
 export default router;
