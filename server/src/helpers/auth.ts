b8000222d7345d1e49349fcb77859b1519bd3f4e
Matthew Alexander <alexandermatthew12@gmail.com>
2020-10-07 16:17:26 +0800

    Cleaned-up styling based on es-lint

--- server/src/helpers/auth.ts
+++ server/src/helpers/auth.ts
@@ -1,13 +1,11 @@
-import { Router } from "express"
-import { withJWTAuthMiddleware } from "express-kun";
-import config from "../config";
+import { Router } from 'express';
+import { withJWTAuthMiddleware } from 'express-kun';
+import config from '../config';
 
-export const accessTokenSecret = 'youraccesstokensecret'; // TODO
+export const accessTokenSecret = 'youraccesstokensecret'; // TODO: @Akhil can we store this access token secret in Google Secret Manager (https://cloud.google.com/secret-manager)
 
 // helper method to create protected router
 // used in routes directories (see e.g. in routes/user.ts)
-export const createProtectedRouter = (router: Router) => {
-    return config.disableAuthentication && config.env != "production" // production must have protected routes
-        ? router
-        : withJWTAuthMiddleware(router, accessTokenSecret);
-};
+export const createProtectedRouter = (router: Router) => (config.disableAuthentication && config.env !== 'production' // production must have protected routes
+  ? router
+  : withJWTAuthMiddleware(router, accessTokenSecret));
