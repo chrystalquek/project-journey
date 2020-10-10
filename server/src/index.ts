e34eb080ad14fa744ffb517222c5d33e247b9b9d
Kendrick Ang <kendrick.wh@outlook.com>
2020-10-10 11:34:45 +0800

    fix dotenv parsing bug

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import expressValidator from 'express-validator';
 import bodyParser from 'body-parser';
+
+// The dotenv file should be parsed before any imports requiring process.env
+// tslint:disable-next-line:no-var-requires
+const result = require('dotenv').config();
+if (result.error) {
+  throw new Error("Error parsing dotenv");
+}
+
 import CONFIG from './config/index';
 import db from './loaders/connection';
 
+import console from 'console';
+
 // Import routes
 import router from './routes';
-
-require('dotenv').config();
 
 // load db -> find better way instead of this
 db;
@@ -20,6 +28,7 @@ app.use(expressValidator());
 // Add routes to app
 app.use('/', router);
 
+console.log(CONFIG.port)
 app.listen(CONFIG.port, () => {
   console.log('listening on port: ', CONFIG.port);
 });
