df4e7993e5b67698d55721f29798ced4d6824d1c
Clara <claraadora@gmail.com>
2020-10-21 19:52:43 +0800

    Add services for Opportunity

---
+++ server/src/services/opportunity.ts
@@ -0,0 +1,84 @@
+import mongoose from 'mongoose';
+import { OpportunityData } from '../types';
+import Opportunity from '../models/Opportunity';
+
+const createOpportunity = async (opportunityData: OpportunityData): Promise<void> => {
+  try {
+    const opportunitySchemeData = new Opportunity({
+      // Inherited
+      _id: new mongoose.Types.ObjectId(),
+      name: opportunityData.name,
+      created_at: Date.now(),
+      description: opportunityData.description,
+      content_url: opportunityData.contentUrl,
+      content_type: opportunityData.contentType,
+      facilitator_name: opportunityData.facilitatorName,
+      facilitator_description: opportunityData.facilitatorDescription,
+      start_date: opportunityData.startDate,
+      end_date: opportunityData.endDate,
+      location: opportunityData.location,
+      deadline: opportunityData.deadline,
+      additional_information: opportunityData.additionalInformation,
+      capacity: opportunityData.capacity,
+      volunteers: opportunityData.volunteers,
+
+      // Specific
+      photo: opportunityData.photo,
+      positions: opportunityData.positions,
+
+    });
+    await opportunitySchemeData.save();
+  } catch (err) {
+    throw new Error(err.msg);
+  }
+};
+
+const readOpportunity = async (id: string) => {
+  try {
+    const opportunity = await Opportunity.findById(id);
+
+    if (!opportunity) {
+      throw new Error('Opportunity is not found.');
+    }
+
+    return opportunity;
+  } catch (err) {
+    throw new Error(err.msg);
+  }
+};
+
+const updateOpportunity = async (
+  id: string,
+  updatedFields: OpportunityData,
+): Promise<void> => {
+  try {
+    await Opportunity.findOneAndUpdate(
+      { _id: id },
+      { $set: updatedFields },
+      { new: true },
+    );
+  } catch (err) {
+    throw new Error(err.msg);
+  }
+};
+
+const deleteOpportunity = async (id: string): Promise<void> => {
+  try {
+    const opportunity = await Opportunity.findById(id);
+
+    if (!opportunity) {
+      throw new Error("Opportunity can't be found");
+    }
+
+    await opportunity.remove();
+  } catch (err) {
+    throw new Error(err.msg);
+  }
+};
+
+export default {
+  createOpportunity,
+  readOpportunity,
+  updateOpportunity,
+  deleteOpportunity,
+};
