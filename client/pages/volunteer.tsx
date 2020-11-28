c3266739e75a1a7e3d19c44c0c273de880af69f5
Limminghong <minghonglim@gmail.com>
2020-11-28 12:53:10 +0800

    Hi matt

---
+++ client/pages/volunteer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VolunteerGrid from './volunteer/volunteerGrid'
+
+export default function Volunteer() {
+    return (
+        <VolunteerGrid />
+      );
+  }
---
+++ client/pages/volunteer/volunteerCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../../styles/volunteer.module.css'
+
+type VolunteerCardProps = {
+    image: string;
+    title: string;
+    date: string;
+    time: string;
+}
+
+export default function VolunteerCard(props: VolunteerCardProps) {
+    return (
+        <div className={styles.card}>
+            <img src={props.image} alt="Avatar"></img>
+            <div className={styles.cardContainer}>
+                <span><b>{props.title}</b></span>
+                <span>{props.date}</span>
+                <span>{props.time}</span>
+                <a>&gt; View Details</a>
+            </div>
+        </div>
+      );
+  }
