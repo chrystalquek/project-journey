f9c5ccdaff26a396a1f659228babd90f530a2f2d
Matthew Alexander <alexandermatthew12@gmail.com>
2020-12-06 23:26:54 +0800

    Changed default port to 5000

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,7 +1,7 @@
 import { EnvironmentConstants } from '../types';
 
 const config: EnvironmentConstants = {
-  port: Number(process.env.PORT) || 3000,
+  port: Number(process.env.PORT) || 5000,
   env: process.env.NODE_ENV ?? 'development',
   disableAuthentication: true,
 };
