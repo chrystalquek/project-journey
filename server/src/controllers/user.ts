2ef3f9c72dc8405d21856ad1f4f97f2aae628320
Clara <claraadora@gmail.com>
2020-12-07 21:47:15 +0800

    Sign token with a copy of user object without password

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -54,7 +54,10 @@ const login = async (req: express.Request, res: express.Response) => {
   try {
     const user = await getUser(email);
     if (bcrypt.compareSync(password, user.password)) {
-      const token = jwt.sign({ sub: user._id, role: user.role }, accessTokenSecret, {
+      const userCopy = Object.assign(user);
+      delete userCopy.password;
+
+      const token = jwt.sign(userCopy, accessTokenSecret, {
         expiresIn: '24h',
       });
       res.status(HTTP_CODES.OK).json({
