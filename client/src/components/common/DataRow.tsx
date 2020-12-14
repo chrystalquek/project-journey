56bc144baffef25cb8f38418d0b52cf1c2ff4ac6
Joe <jteddysanjaya@gmail.com>
2020-12-14 16:55:49 +0800

    Refactor components

---
+++ client/src/components/common/DataRow.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+const DataRow = ({ header, data , xs1, xs2}) => (
+  <Grid container item>
+    <Grid item xs={xs1}>
+      <Typography ><strong>{header}:</strong></Typography>
+    </Grid>
+    <Grid item xs={xs2}>
+      <Typography>{data}</Typography>
+    </Grid>
+  </Grid>
+)
+
+export default DataRow;
---
+++ client/src/components/common/ProfileDivider.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Divider from '@material-ui/core/Divider'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  divider: {
+    marginBottom: '10px',
+    marginTop: '5px'
+  }
+}))
+
+const ProfileDivider = () => {
+  const classes = useStyles()
+  return <Divider className={classes.divider}/>
+}
+
+export default ProfileDivider;
+  
