57d13a7c9905484ae914f99619a818248b9ebe33
Joe <jteddysanjaya@gmail.com>
2020-12-14 17:09:11 +0800

    Add missing components

--- client/src/components/EventCount.tsx
+++ client/src/components/EventCount.tsx
@@ -3,8 +3,17 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import ProfileDivider from '@components/common/ProfileDivider'
 import DataRow from '@components/common/DataRow'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    color: '#00BADC'
+  }
+}))
 
 const EventCount = ({ user }) => {
+  const classes = useStyles()
+
   return (
     <Grid style={{padding:'20px 20px'}} container direction='column'>
       <Grid item>
@@ -16,6 +25,9 @@ const EventCount = ({ user }) => {
       <DataRow header='Volunteering Sessions' data={user.volunteeringSessions} xs1={11} xs2={1}/>
       <DataRow header='Workshops' data={user.workshops} xs1={11} xs2={1}/>
       <DataRow header='Hangouts' data={user.hangouts} xs1={11} xs2={1}/>
+      <Grid item>
+        <Typography className={classes.link}><u>View past events</u></Typography>
+      </Grid>
     </Grid>
   )
 }
