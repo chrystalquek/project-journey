db5c4eda777ee8e35e4c215265708db11ee7a258
Akhil Vuputuri <e0309566@u.nus.edu>
2020-12-26 16:58:11 +0800

    Add signupArgs and signup method

--- client/redux/actions/user.ts
+++ client/redux/actions/user.ts
@@ -1,12 +1,54 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import apiClient from '../../api/apiClient';
-import { LoginRequest } from '../../api/request';
-import { LoginResponse } from '../../api/response';
+import { SignupRequest, LoginRequest } from '../../api/request';
+import { SignupResponse, LoginResponse } from '../../api/response';
+
+export type SignupArgs = {
+  name: string
+  email: string
+  password: string
+  gender: string
+  citizenship: string
+  race: string
+  hasVolunteered: boolean
+  hasChildrenExperience: boolean
+  hasExternalVolunteerExperience: boolean
+  hasFirstAidCertification: boolean
+  volunteerFrequency: number
+  volunteerReason: string
+  volunteerContribution: string
+  birthday: Date
+}
 
 export type LoginArgs = {
   email: string,
   password: string
 }
+
+export const signup = createAsyncThunk<SignupResponse, SignupArgs, { state }>(
+  'volunteer/',
+  async ({ name, email, password, gender, citizenship, race,hasVolunteered, hasChildrenExperience, hasExternalVolunteerExperience,hasFirstAidCertification, volunteerFrequency, volunteerReason,volunteerContribution, birthday }) => {
+    const request: SignupRequest = {
+      name,
+      email,
+      password,
+      gender,
+      citizenship,
+      race,
+      hasVolunteered,
+      hasChildrenExperience,
+      hasExternalVolunteerExperience,
+      hasFirstAidCertification,
+      volunteerFrequency,
+      volunteerReason,
+      volunteerContribution,
+      birthday
+    };
+
+    const response = await apiClient.signup(request) as SignupResponse;
+    return response;
+  },
+);
 
 const login = createAsyncThunk<LoginResponse, LoginArgs, { state }>(
   'user/login',
