06bb0a2e683962b0e15b0627db833a913743a995
Akhil Vuputuri <e0309566@u.nus.edu>
2020-12-26 16:56:59 +0800

    Add signupRequest type

--- client/api/request.ts
+++ client/api/request.ts
@@ -1,3 +1,20 @@
+export type SignupRequest = {
+  name: string
+  email: string
+  password: string
+  gender: string
+  citizenship: string
+  race: string
+  hasVolunteered: boolean
+  hasChildrenExperience: boolean
+  hasExternalVolunteerExperience: boolean
+  hasFirstAidCertification: boolean
+  volunteerFrequency: number
+  volunteerReason: string
+  volunteerContribution: string
+  birthday: Date
+}
+
 export type LoginRequest = {
   email: string
   password: string
