6a4323389f7a2b6df033a0829aca7e60aacebfe7
Akhil Vuputuri <e0309566@u.nus.edu>
2020-12-31 18:18:09 +0800

    Replace AntD divider w custom component

--- client/src/components/user/Signup.tsx
+++ client/src/components/user/Signup.tsx
@@ -1,9 +1,9 @@
 import {
-  Box, Grid, Button, TextField, Typography
+  Box, Grid, Button, TextField, Typography, Divider as Divider1
 } from '@material-ui/core'
 import React, { FC, useEffect, useState } from 'react';
 import {
-  Layout, Divider, Card,
+ Divider, Card,
 } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
 import Head from 'next/head';
@@ -57,11 +57,17 @@ const useStyles = makeStyles((theme) => ({
   },
   invalidText: {
     marginBottom: "10px",
-    color: "#e60026",
+    color: "#e60026"
   },
+  dividerLine: {
+    backgroundColor: '#D0DE39',
+    height: 1
+  },
+  dividerText: {
+    fontSize: 16,
+    fontWeight: 500
+  }
 }));
-
-const { Content } = Layout;
 
 type SignupProps = {
   user: UserState;
@@ -127,94 +133,20 @@ const Signup: FC<SignupProps> = ({ user, handleFormSubmit }: SignupProps) => {
     }
   };
 
-  const VolunteerType = (props) => (
-    <Layout>
-      <Content
-        style={{
-          padding: "80px 150px 0px 150px",
-          marginTop: 80,
-          textAlign: "center",
-          minHeight: "90vh",
-        }}
-        >
-          <div>
-            <Grid container style={{ justifyContent: 'center', textAlign: 'left' }}>
-              <Grid item xs={6}>
-                <p>
-                  Yay! We are excited that you are interested to volunteer with us.
-                  (information to help the users make a decision)
-                  Please be reminded that there is a minimum commitment of 3 months
-                  (serving a minimum of 3 sessions a month) in order to have direction
-                  interaction with our learning community (aged 6 to 16 years old).
-                  We have this policy for a number of reasons:
-                  <ul>
-                    <li>
-                      Limit the emotional trauma in our children that occurs
-                      when volunteers come for a few sessions and leave
-                    </li>
-                    <li>
-                      It takes time for children to warm up to
-                      new faces and it won&apos;t happen instantly.
-                    </li>
-                    <li>
-                      3 months provides you with the opportunity to witness the progression
-                      and impact you&apos;re making (in yourself and in our children)
-                    </li>
-                    <li>
-                      You&apos;ll be journeying with other individuals who share
-                      a commitment and passion for our program.  There are opportunities
-                      to meet new people and develop friendships.
-                    </li>
-                    <li>
-                      Allows you time to be on-boarded, mentored
-                      and guided by some incredible WCA Captains on the team.
-                    </li>
-                  </ul>
-                </p>
-              </Grid>
-            </Grid>
-          </div>
-          <div>
-            <Grid container style={{ justifyContent: 'center' }}>
-              <Grid item xs={6}>
-                <Divider style={{ borderColor: '#D0DE39', borderWidth: 3 }}>
-                  Sign Up As
-                </Divider>
-              </Grid>
-            </Grid>
-            <div style={{ padding: 24 }}>
-              <Grid container spacing={6} style={{ justifyContent: 'center' }}>
-                <Grid item xs={3}>
-                  <Card hoverable title="Ad-hoc Volunteer" headStyle={{ background: '#D0DE39', color: '#fff', borderRadius: '10px 10px 0px 0px' }} bordered={false} style={{ borderRadius: '10px' }} onClick={selectAdhoc}>
-                    You are only intending to volunteer one-off at Blessings in a Bag
-                  </Card>
-                </Grid>
-                <Grid item xs={3}>
-                  <Card hoverable title="Regular Volunteer" headStyle={{ background: '#00BADC', color: '#fff', borderRadius: '10px 10px 0px 0px' }} bordered={false} style={{ borderRadius: '10px' }} onClick={selectCommitted}>
-                    You are able to commit to a minimum of 3 months at Blessings in a Bag
-                  </Card>
-                </Grid>
-              </Grid>
-            </div>
-          </div>
-  
-          <div className="section">
-            Already have an account?
-            {' '}
-            <br />
-            <Link href="/auth/login">
-              Log in
-            </Link>
-          </div>
-        </Content>
-      </Layout>
-    );
-  }
-
-  const VolunteerType1 = props => {
+  const TextDivider = ({ children }) => {
     return (
-      <Layout>
-        <Content style={{
+      <Grid container>
+        <Grid item xs={4} ><Divider1 className ={classes.dividerLine}/></Grid>
+        <Grid item xs={4} className ={classes.dividerText}>{children}</Grid>
+        <Grid item xs={4} className ={classes.dividerLine}><Divider1 /></Grid>
+      </Grid>
+    );
+  };
+
+  const VolunteerType = props => {
+    return (
+      <Box>
+        <Box style={{
           padding: '80px 150px 0px 150px', marginTop: 80, textAlign: 'center', minHeight: '90vh',
         }}
         >
@@ -258,9 +190,9 @@ const Signup: FC<SignupProps> = ({ user, handleFormSubmit }: SignupProps) => {
           <div>
             <Grid container style={{ justifyContent: 'center' }}>
               <Grid item xs={6}>
-                <Divider style={{ borderColor: '#D0DE39', borderWidth: 3 }}>
-                  Sign Up As
-                </Divider>
+                <TextDivider>
+                  Sign  Up   As
+                </TextDivider>
               </Grid>
             </Grid>
             <div style={{ padding: 24 }}>
@@ -287,7 +219,10 @@ const Signup: FC<SignupProps> = ({ user, handleFormSubmit }: SignupProps) => {
               Log in
             </Link>
           </div>
-        </div>
+        </Box>
+      </Box>
+    );
+  }
 
         <div className="section">
           Already have an account? <br />
@@ -419,7 +354,7 @@ const Signup: FC<SignupProps> = ({ user, handleFormSubmit }: SignupProps) => {
 
   const VolunteerSignup = (props) => {
     if (currentStep == 0) {
-      return <div><VolunteerType1 /><VolunteerType /></div>
+      return <div><VolunteerType /></div>
     } else {
       return <VolunteerInfo />
     }
