c118d7b3bc0e002dd6850c92c81f71b2968a4b0a
Akhil Vuputuri <e0309566@u.nus.edu>
2021-01-04 00:16:12 +0800

    Add image mongo model

---
+++ server/src/models/Image.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { ImageData } from '../types';
+
+const { Schema } = mongoose;
+
+export type ImageModel = ImageData & mongoose.Document;
+
+const ImageSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  email: String,
+  imageName: String,
+  created_at: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+export default mongoose.model<ImageModel>('Image', ImageSchema);
