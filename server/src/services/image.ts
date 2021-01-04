764cfb576c702b322e6ce9bcc996aad78b77ad67
Akhil Vuputuri <e0309566@u.nus.edu>
2021-01-04 15:17:54 +0800

    Remove duplicates before adding new entry into mongo

--- server/src/services/image.ts
+++ server/src/services/image.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
-import { ImageData } from '../types';
+import { ImageData, ImageResponse } from '../types';
 import Image from '../models/Image';
 import { Storage } from '@google-cloud/storage';
 
 const storage = new Storage()
 
-const uploadImage = async (imageData: ImageData): Promise<void> => {
+const uploadImage = async (imageData: ImageData): Promise<ImageResponse> => {
   try {
     const filepath = "./uploads/" + imageData.imageName
     const returnVal = await storage.bucket('journey-storage').upload(filepath, {
@@ -16,13 +16,26 @@ const uploadImage = async (imageData: ImageData): Promise<void> => {
     });
     try {
       const url = returnVal[1].mediaLink;
+      
+      await Image.deleteMany({
+        email: imageData.email
+      })
+
       const imageSchemaData = new Image({
       _id: new mongoose.Types.ObjectId(),
       email: imageData.email,
       imageName: imageData.imageName,
       url: url
     });
+    
+      const imageResponse: ImageResponse = {
+        email: imageData.email,
+        imageName: imageData.imageName,
+        url: url
+      };
+
     await imageSchemaData.save().then(created => console.info(`Created ${created}`));
+    return imageResponse;
     } catch (err) {
       throw new Error(err.msg)
     }
@@ -31,6 +44,21 @@ const uploadImage = async (imageData: ImageData): Promise<void> => {
   }
 };
 
+const getImage = async (email: string) => {
+  const image = await Image.findOne({
+    email,
+  }).lean().exec();
+
+  if (!image) {
+    throw new Error(`Image with email: ${email} not found`);
+  }
+
+  console.log(JSON.stringify(image))
+
+  return "hello";
+};
+
 export default {
-  uploadImage
+  uploadImage,
+  getImage
 };
