0d89a681c27f2c412cca51889dbd143a27ccdea3
DystoriaX <dascogabriel@gmail.com>
2021-01-05 22:35:37 +0800

    Add features: disable button and multiline displayText

--- client/src/components/form/signup-questions/SignUpFormGenerator.tsx
+++ client/src/components/form/signup-questions/SignUpFormGenerator.tsx
@@ -1,10 +1,6 @@
 import { Fragment, useCallback } from 'react';
-import {
-  Paper, Typography, MenuItem, Button,
-} from '@material-ui/core';
-import {
-  Formik, Field, Form, ErrorMessage,
-} from 'formik';
+import { Paper, Typography, MenuItem, Button } from '@material-ui/core';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { TextField, CheckboxWithLabel } from 'formik-material-ui';
 import { DatePicker } from 'formik-material-ui-pickers';
 import { QuestionList, OptionType, InputType } from '@type/questions';
@@ -13,9 +9,9 @@ import DateFnsUtils from '@date-io/date-fns';
 import * as Yup from 'yup';
 
 type FormGeneratorProps = {
-  questionList: QuestionList
-  handleSignUp: (formValues: Record<string, any>) => void
-}
+  questionList: QuestionList;
+  handleSignUp: (formValues: Record<string, any>) => void;
+};
 
 const FormGenerator = ({ questionList, handleSignUp }: FormGeneratorProps) => {
   const initialValues: Record<string, any> = {};
@@ -24,9 +20,12 @@ const FormGenerator = ({ questionList, handleSignUp }: FormGeneratorProps) => {
     initialValues[name] = type === 'checkboxes' ? [] : initialValue;
   });
 
-  const handleSubmit = useCallback((values: Record<string, any>) => {
-    handleSignUp(values);
-  }, [questionList]);
+  const handleSubmit = useCallback(
+    (values: Record<string, any>) => {
+      handleSignUp(values);
+    },
+    [questionList]
+  );
 
   const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
@@ -39,28 +38,35 @@ const FormGenerator = ({ questionList, handleSignUp }: FormGeneratorProps) => {
       is: (val: string) => val && val.length > 0,
       then: Yup.string().oneOf(
         [Yup.ref('password')],
-        'Confirm Password need to be the same as Password fields',
+        'Confirm Password need to be the same as Password fields'
       ),
     }),
-    mobileNumber: Yup.string().matches(phoneRegExp, 'Mobile phone is not valid').required(),
+    mobileNumber: Yup.string()
+      .matches(phoneRegExp, 'Mobile phone is not valid')
+      .required(),
     institutionName: Yup.string().required(),
     position: Yup.string().required(),
   });
 
-  const FormQuestionMapper = ({ formType, name, options }: {
-    formType: InputType,
-    name: string,
-    options: OptionType[] | null
+  const FormQuestionMapper = ({
+    formType,
+    name,
+    options,
+  }: {
+    formType: InputType;
+    name: string;
+    options: OptionType[] | null;
   }) => {
     switch (formType) {
       case 'date':
         return (
           <Field
             component={DatePicker}
-            inputVariant="outlined"
-            format="dd/MM/yyyy"
+            inputVariant='outlined'
+            format='dd/MM/yyyy'
             name={name}
             fullWidth
+            margin='dense'
           />
         );
       case 'shortAnswer':
@@ -68,67 +74,75 @@ const FormGenerator = ({ questionList, handleSignUp }: FormGeneratorProps) => {
         return (
           <Field
             component={TextField}
-            variant="outlined"
+            variant='outlined'
             type={formType === 'password' ? formType : 'text'}
             name={name}
             fullWidth
+            margin='dense'
           />
         );
       case 'longAnswer':
         return (
           <Field
             component={TextField}
-            variant="outlined"
+            variant='outlined'
             name={name}
             fullWidth
             multiline
+            margin='dense'
           />
         );
       case 'mcq':
         return (
           <Field
             component={TextField}
-            variant="outlined"
+            variant='outlined'
             name={name}
             fullWidth
             select
             InputLabelProps={{
               shrink: true,
             }}
+            margin='dense'
           >
-            {(options as Array<OptionType>).map(
-              ({ value, label }) => (
-                <MenuItem key={value} value={value}>
-                  {label}
-                </MenuItem>
-              ),
-            )}
+            {(options as Array<OptionType>).map(({ value, label }) => (
+              <MenuItem key={value} value={value}>
+                {label}
+              </MenuItem>
+            ))}
           </Field>
         );
       case 'checkboxes':
       default:
         return (
-          <>
-            {(options as Array<OptionType>).map(
-              ({ value, label }) => (
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {(options as Array<OptionType>).map(({ value, label }) => (
+              <div style={{ flex: 1 }}>
                 <Field
                   component={CheckboxWithLabel}
                   name={name}
                   key={value}
                   value={value}
                   Label={{ label }}
-                  color="primary"
-                  type="checkbox"
+                  color='primary'
+                  type='checkbox'
                 />
-              ),
-            )}
-          </>
+              </div>
+            ))}
+          </div>
         );
     }
   };
 
   return (
-    <Paper style={{ width: '500px', margin: 'auto', padding: '30px' }}>
+    <Paper
+      style={{
+        width: '500px',
+        margin: 'auto',
+        padding: '30px',
+        textAlign: 'left',
+      }}
+    >
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Formik
           initialValues={initialValues}
@@ -136,43 +150,55 @@ const FormGenerator = ({ questionList, handleSignUp }: FormGeneratorProps) => {
           validationSchema={SignUpSchema}
           validateOnChange={false}
         >
-          {({
-            isSubmitting,
-            touched,
-            values,
-            errors,
-          }) => (
-
+          {({ isSubmitting, touched, values, errors }) => (
             <Form>
               {questionList.map((questionItem) => {
                 const { name, displayText, type } = questionItem;
-                const options = type === 'mcq' || type === 'checkboxes'
-                  ? questionItem.options
-                  : null;
+                const options =
+                  type === 'mcq' || type === 'checkboxes'
+                    ? questionItem.options
+                    : null;
 
                 return (
                   <div
-                    key={questionItem.displayText}
+                    key={questionItem.displayText[0]}
                     style={{
                       marginBottom: '32px',
                     }}
                   >
-                    <Typography style={{
-                      marginBottom: '16px',
-                    }}
-                    >
-                      {displayText}
-                    </Typography>
-                    <FormQuestionMapper formType={type} name={name} options={options} />
+                    {displayText.map((text) => {
+                      return (
+                        <Typography
+                          key={text}
+                          style={{
+                            marginBottom: '16px',
+                            fontWeight: '600',
+                          }}
+                        >
+                          {text}
+                        </Typography>
+                      );
+                    })}
+                    <FormQuestionMapper
+                      formType={type}
+                      name={name}
+                      options={options}
+                    />
                   </div>
                 );
               })}
               <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                disabled={isSubmitting}
-                size="large"
+                variant='contained'
+                color='primary'
+                type='submit'
+                disabled={
+                  isSubmitting ||
+                  values.permissionEmailCollection === 'no' ||
+                  values.personalInformationConsent === 'no' ||
+                  values.acknowledgeTnC === 'no' ||
+                  values.informedConsent === 'no'
+                }
+                size='large'
               >
                 Submit
               </Button>
--- client/src/components/form/signup-questions/SignUpQuestionList.tsx
+++ client/src/components/form/signup-questions/SignUpQuestionList.tsx
@@ -3,44 +3,45 @@ import { QuestionList } from '@type/questions';
 export const questions: QuestionList = [
   {
     name: 'firstName',
-    displayText: 'First Name',
+    displayText: ['First Name'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'lastName',
-    displayText: 'Last Name (Family Name)',
+    displayText: ['Last Name (Family Name)'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'email',
-    displayText: 'Email',
+    displayText: ['Email'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'password',
-    displayText: 'Password',
+    displayText: ['Password'],
     type: 'password',
     initialValue: '',
   },
   {
     name: 'confirmPassword',
-    displayText: 'Confirm password',
+    displayText: ['Confirm password'],
     type: 'password',
     initialValue: '',
   },
   {
     name: 'nickname',
-    displayText:
+    displayText: [
       'Do you go by any aliases/nicknames? If so, please state below',
+    ],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'gender',
-    displayText: 'Gender',
+    displayText: ['Gender'],
     type: 'mcq',
     initialValue: 'male',
     options: [
@@ -50,7 +51,7 @@ export const questions: QuestionList = [
   },
   {
     name: 'citizenship',
-    displayText: 'Citizenship',
+    displayText: ['Citizenship'],
     type: 'mcq',
     initialValue: 'singapore',
     options: [
@@ -64,43 +65,43 @@ export const questions: QuestionList = [
   },
   {
     name: 'birthday',
-    displayText: 'Date of Birth',
+    displayText: ['Date of Birth'],
     type: 'date',
     initialValue: new Date(),
   },
   {
     name: 'mobileNumber',
-    displayText: 'Mobile Number',
+    displayText: ['Mobile Number'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'email',
-    displayText: 'Email Address',
+    displayText: ['Email Address'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'instagramHandle',
-    displayText: 'What is your instagram handle?',
+    displayText: ['What is your instagram handle?'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'institutionName',
-    displayText: 'School / company name',
+    displayText: ['School / company name'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'position',
-    displayText: 'Year Level / Position',
+    displayText: ['Year Level / Position'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'race',
-    displayText: 'Race / Ethnicity',
+    displayText: ['Race / Ethnicity'],
     type: 'mcq',
     options: [
       { value: 'chinese', label: 'Chinese' },
@@ -113,17 +114,18 @@ export const questions: QuestionList = [
   },
   {
     name: 'language',
-    displayText:
+    displayText: [
       'What languages do you speak (please put your first language at the start of your response)',
+    ],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     // Any other name suggestion?
     name: 'howKnow',
-    displayText: 'How did you hear about us/Beyond Awesome?',
+    displayText: ['How did you hear about us/Beyond Awesome?'],
     type: 'checkboxes',
-    initialValue: 'other',
+    initialValue: [],
 
     // Need clarification for the value assigned
     options: [
@@ -142,8 +144,9 @@ export const questions: QuestionList = [
   },
   {
     name: 'volunteeredBefore',
-    displayText:
+    displayText: [
       'Have you volunteered with us before? (eg ad-hoc volunteer or other event)',
+    ],
     type: 'mcq',
     initialValue: 'yes',
     options: [
@@ -153,8 +156,9 @@ export const questions: QuestionList = [
   },
   {
     name: 'whyInterested',
-    displayText:
+    displayText: [
       "Please tell us why you're interested in volunteering with our program",
+    ],
     type: 'longAnswer',
     initialValue: '',
   },
@@ -162,23 +166,26 @@ export const questions: QuestionList = [
   // Medical Information
   {
     name: 'medicalNeeds',
-    displayText:
+    displayText: [
       'Do you have medical needs we should be aware of (e.g., heart conditions, asthma, seizures, diabetes, hearing or sight loss, etc.)?\nIf yes, please explain',
+    ],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'allergies',
-    displayText:
+    displayText: [
       'Do you have any allergies (medications, foods, materials)?\nIf yes, please explain',
+    ],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     // Any better name suggestion?
     name: 'medicalAwareness',
-    displayText:
+    displayText: [
       'Do you take any medication during the day that we should be aware of?',
+    ],
     type: 'mcq',
     initialValue: 'no',
     options: [
@@ -190,25 +197,25 @@ export const questions: QuestionList = [
   // Emergency Contact Information
   {
     name: 'emergencyContactNo',
-    displayText: 'Emergency Contact Mobile Number #1',
+    displayText: ['Emergency Contact Mobile Number #1'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'emergencyContactEmail',
-    displayText: 'Emergency Contact Email Address #1',
+    displayText: ['Emergency Contact Email Address #1'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'emergencyContactRelation',
-    displayText: 'Emergency Contact Relationship',
+    displayText: ['Emergency Contact Relationship'],
     type: 'shortAnswer',
     initialValue: '',
   },
   {
     name: 'emergencyContactOther',
-    displayText: 'If Other, please specify',
+    displayText: ['If Other, please specify'],
     type: 'shortAnswer',
     initialValue: '',
   },
@@ -216,8 +223,9 @@ export const questions: QuestionList = [
   // Permission for Email Collection and Communication
   {
     name: 'permissionEmailCollection',
-    displayText:
+    displayText: [
       'I have read all of the above information, and hereby acknowledge and accept the terms and conditions set forth herein.',
+    ],
     type: 'mcq',
     initialValue: 'no',
     options: [
@@ -229,8 +237,9 @@ export const questions: QuestionList = [
   // Acknowledgements
   {
     name: 'personalInformationConsent',
-    displayText:
+    displayText: [
       'I give Beyond Awesome (A program by Blessings in a Bag Limited) to communicate my personal information to volunteer mentors in order to support my experience, development and journey with the program',
+    ],
     type: 'mcq',
     initialValue: 'no',
     options: [
@@ -240,8 +249,9 @@ export const questions: QuestionList = [
   },
   {
     name: 'acknowledgeTnC',
-    displayText:
+    displayText: [
       'I have read all of the above information, and hereby acknowledge and accept the terms and conditions set forth herein',
+    ],
     type: 'mcq',
     initialValue: 'no',
     options: [
@@ -253,13 +263,17 @@ export const questions: QuestionList = [
   // Informed Consent and License Agreement
   {
     name: 'informedConsent',
-    displayText: `"PUBLISHING PERMISSION: This form grants Beyond Awesome (of Blessings in a Bag Limited) the right to edit and publish the above named individual's work (including writing, audio recordings, and any other products) in any manner in any type of publication or materials affiliated with Beyond Awesome (of Blessings in a Bag Limited), now existing or which may be created in the future. This is a non-exclusive agreement, meaning that the author can also publish their work in publications unaffiliated with Beyond Awesome (of Blessings in a Bag Limited), as well. Publication by another person or entity shall not have any effect on Beyond Awesome (of Blessings in a Bag Limited) rights under this agreement. The author warrants to Beyond Awesome (of Blessings in a Bag Limited) that to the extent the author's material contains any accounts of non-fictional individuals or events, that the information is substantially true, does not defame or slander or place in a false light any individual or group, and does not disclose private or personal facts regarding any third party without that third party's written consent. Beyond Awesome (of Blessings in a Bag Limited) also asks students to refrain from writing inappropriate language and about incidents that may hurt, degrade, or endanger others and the author agrees to advise Beyond Awesome (of Blessings in a Bag Limited) if its work is not in compliance with this request. We do retain the right to paraphrase or re-word passages during the editing process. FILM, PHOTOGRAPHY, AND AUDIO PERMISSION: During Beyond Awesome (of Blessings in a Bag Limited) programming, Beyond Awesome (of Blessings in a Bag Limited) may document the work of the individual through photography, film, video, audio, or other recording technique. Individuals agree that these images and recordings may thereafter appear in Beyond Awesome (of Blessings in a Bag Limited) publications, reports, film productions, websites, or social or online media platforms. This documentation is intended to display the hard work of the individuals and the type and effectiveness of the programming Beyond Awesome (of Blessings in a Bag Limited). No material will be used for commercial purposes but rather will be used for furthering the Beyond Awesome (of Blessings in a Bag Limited) goals of supporting and serving under-resourced students. Consent to such use, including any waiver of any right to publicity or privacy, is given. 
-      
-      MEDIA PERMISSION: While individuals are engaged in programming at Beyond Awesome (of Blessings in a Bag Limited), they may appear in photographs, video or audio that Beyond Awesome (of Blessings in a Bag Limited) may wish to use on the radio, television, in press articles, or other media about Beyond Awesome (of Blessings in a Bag Limited). These materials help provide the public with information about programs at Beyond Awesome (of Blessings in a Bag Limited) and allow individuals the opportunity to have their work shared with a broader audience. Consent to such use, including any waiver of any right to publicity or privacy, is given. 
-      
-      FEEDBACK PERMISSION: In order to gain information about the effectiveness of our programs and better address the needs of students, communities and the program in general, Beyond Awesome (of Blessings in a Bag Limited) may request access for feedback and survey completion by individuals, including (but not limited to): focus groups, online surveys, feedback forms, reflection sheets, etc.  At no point is participation in any of these required and no identifying information will be attached to any results that are collected or disseminated unless otherwise confirmed or agreed upon between the individual and the organisation. Findings will be shared for the purposes of demonstrating our programs and their impact to funders and partners. Furthermore, Beyond Awesome (of Blessings in a Bag Limited) reserves the right to communicate within leadership regarding individual achievement, development and well-being. 
-      
-      Please note that although participation in Beyond Awesome (of Blessings in a Bag Limited) programs is voluntary, once this agreement is signed, it cannot be withdrawn and remains in effect even after the individual is no longer participating in any Beyond Awesome (of Blessings in a Bag Limited) programming. "`,
+    displayText: [
+      `"PUBLISHING PERMISSION: This form grants Beyond Awesome (of Blessings in a Bag Limited) the right to edit and publish the above named individual's work (including writing, audio recordings, and any other products) in any manner in any type of publication or materials affiliated with Beyond Awesome (of Blessings in a Bag Limited), now existing or which may be created in the future. This is a non-exclusive agreement, meaning that the author can also publish their work in publications unaffiliated with Beyond Awesome (of Blessings in a Bag Limited), as well. Publication by another person or entity shall not have any effect on Beyond Awesome (of Blessings in a Bag Limited) rights under this agreement. The author warrants to Beyond Awesome (of Blessings in a Bag Limited) that to the extent the author's material contains any accounts of non-fictional individuals or events, that the information is substantially true, does not defame or slander or place in a false light any individual or group, and does not disclose private or personal facts regarding any third party without that third party's written consent. Beyond Awesome (of Blessings in a Bag Limited) also asks students to refrain from writing inappropriate language and about incidents that may hurt, degrade, or endanger others and the author agrees to advise Beyond Awesome (of Blessings in a Bag Limited) if its work is not in compliance with this request. We do retain the right to paraphrase or re-word passages during the editing process.`,
+
+      `FILM, PHOTOGRAPHY, AND AUDIO PERMISSION: During Beyond Awesome (of Blessings in a Bag Limited) programming, Beyond Awesome (of Blessings in a Bag Limited) may document the work of the individual through photography, film, video, audio, or other recording technique. Individuals agree that these images and recordings may thereafter appear in Beyond Awesome (of Blessings in a Bag Limited) publications, reports, film productions, websites, or social or online media platforms. This documentation is intended to display the hard work of the individuals and the type and effectiveness of the programming Beyond Awesome (of Blessings in a Bag Limited). No material will be used for commercial purposes but rather will be used for furthering the Beyond Awesome (of Blessings in a Bag Limited) goals of supporting and serving under-resourced students. Consent to such use, including any waiver of any right to publicity or privacy, is given.`,
+
+      `MEDIA PERMISSION: While individuals are engaged in programming at Beyond Awesome (of Blessings in a Bag Limited), they may appear in photographs, video or audio that Beyond Awesome (of Blessings in a Bag Limited) may wish to use on the radio, television, in press articles, or other media about Beyond Awesome (of Blessings in a Bag Limited). These materials help provide the public with information about programs at Beyond Awesome (of Blessings in a Bag Limited) and allow individuals the opportunity to have their work shared with a broader audience. Consent to such use, including any waiver of any right to publicity or privacy, is given.`,
+
+      `FEEDBACK PERMISSION: In order to gain information about the effectiveness of our programs and better address the needs of students, communities and the program in general, Beyond Awesome (of Blessings in a Bag Limited) may request access for feedback and survey completion by individuals, including (but not limited to): focus groups, online surveys, feedback forms, reflection sheets, etc.  At no point is participation in any of these required and no identifying information will be attached to any results that are collected or disseminated unless otherwise confirmed or agreed upon between the individual and the organisation. Findings will be shared for the purposes of demonstrating our programs and their impact to funders and partners. Furthermore, Beyond Awesome (of Blessings in a Bag Limited) reserves the right to communicate within leadership regarding individual achievement, development and well-being.`,
+
+      `Please note that although participation in Beyond Awesome (of Blessings in a Bag Limited) programs is voluntary, once this agreement is signed, it cannot be withdrawn and remains in effect even after the individual is no longer participating in any Beyond Awesome (of Blessings in a Bag Limited) programming. "`,
+    ],
     type: 'mcq',
     initialValue: 'no',
     options: [
