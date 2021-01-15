17caa395a6543dbad07c6e4d70fa53de0ca20a69
Matthew Alexander <alexandermatthew12@gmail.com>
2021-01-15 14:54:55 +0800

    Fixed loading progress

--- client/pages/profile/[volunteer_id].tsx
+++ client/pages/profile/[volunteer_id].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CircularProgress, Grid } from '@material-ui/core';
 import ProfileHeader from '@components/profile/ProfileHeader';
 import Remarks from '@components/profile/Remarks';
@@ -17,10 +17,17 @@ const Profile = () => {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  const profilePageId = router.query.volunteer_id as string
+  const profilePageId = router.query.volunteer_id as string;
 
   const loggedInUser = useSelector((state: StoreState) => state.user);
-  const profilePageData = useSelector((state: StoreState) => state.profilePage.data);
+  // @ts-ignore need to fix the payload struct here on the reducer side
+  // TODO: @teddy
+  const profilePageData = useSelector((state: StoreState) => state.profilePage.data.data);
+
+  // First time load page, send request to receive data
+  useEffect(() => {
+    dispatch(getVolunteerById(profilePageId));
+  }, []);
 
   // Guard clause, user cannot view other users
   // Only admin can view other users
@@ -30,41 +37,37 @@ const Profile = () => {
     return <ErrorPage statusCode={404} />;
   }
 
-  // Get the profilePageData if it's not loaded yet
-  if (profilePageData === null || profilePageData?._id !== profilePageId) {
-    dispatch(getVolunteerById(profilePageId))
-    return <CircularProgress />
-  }
-
-  return (
-    <Grid container direction="column">
-      <Grid item>
-        <NavBar userData={profilePageData} />
-      </Grid>
-      <Grid item container>
-        <Grid item md={2} />
-        <Grid item container direction="column" xs={12} md={8}>
-          <Grid item>
-            <ProfileHeader user={profilePageData} />
-          </Grid>
-          <Grid item container>
-            <Grid item xs={12} md={7}>
-              <Remarks user={profilePageData} />
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <ContactInformation user={profilePageData} />
-            </Grid>
-            <Grid item xs={12} md={7}>
-              <SignUpInformation user={profilePageData} />
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <EventCount user={profilePageData} />
-            </Grid>
-          </Grid>
+  return (profilePageData === null || profilePageData?._id !== profilePageId ? <CircularProgress />
+    : (
+      <Grid container direction="column">
+        <Grid item>
+          <NavBar userData={profilePageData} />
         </Grid>
-        <Grid item md={2} />
+        <Grid item container>
+          <Grid item md={2} />
+          <Grid item container direction="column" xs={12} md={8}>
+            <Grid item>
+              <ProfileHeader user={profilePageData} />
+            </Grid>
+            <Grid item container>
+              <Grid item xs={12} md={7}>
+                <Remarks user={profilePageData} />
+              </Grid>
+              <Grid item xs={12} md={5}>
+                <ContactInformation user={profilePageData} />
+              </Grid>
+              <Grid item xs={12} md={7}>
+                <SignUpInformation user={profilePageData} />
+              </Grid>
+              <Grid item xs={12} md={5}>
+                <EventCount user={profilePageData} />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item md={2} />
+        </Grid>
       </Grid>
-    </Grid>
+    )
   );
 };
 export default Profile;
