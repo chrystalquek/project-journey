440075f097bcbeab2a208d14c59e41089c90e735
Joe <jteddysanjaya@gmail.com>
2021-01-25 11:18:03 +0800

    Finish setting up cropper

--- client/src/components/profile/ProfilePicture.tsx
+++ client/src/components/profile/ProfilePicture.tsx
@@ -4,6 +4,8 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { PhotoCamera } from '@material-ui/icons';
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
+import DiscardSaveButtons from './DiscardSaveButtons';
+import { string } from 'yup';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -22,16 +24,15 @@ const useStyles = makeStyles((theme) => ({
   input: {
     display: 'none'
   },
-  image: {
-    maxWidth: '50%',
-  },
 }))
 
 const ProfilePicture = ({ profilePageData }) => {
   const classes = useStyles();
 
+  var fileUrl = "";
   const [src, setSrc] = useState(null);
   const [imageRef, setImageRef] = useState(null);
+  const [newImageUrl, setNewImageUrl] = useState("");
   const [crop, setCrop] = useState({
     unit: '%',
     width: 30,
@@ -40,7 +41,6 @@ const ProfilePicture = ({ profilePageData }) => {
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
-
 
   const onSelectFile = (e) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -53,13 +53,13 @@ const ProfilePicture = ({ profilePageData }) => {
   }
 
   const onImageLoaded = (image) => {
-    console.log(`onImageLoaded called ${image}`)
+    console.log(`onImageLoaded called`)
     setImageRef(image);
   }
 
   const onCropComplete = (newCrop) => {
     console.log('onCropComplete called')
-    console.log(newCrop)
+    makeClientCrop(newCrop);
   }
 
   const onCropChange = (newCrop) => {
@@ -67,10 +67,56 @@ const ProfilePicture = ({ profilePageData }) => {
     setCrop(newCrop)
   }
 
-  const handleClose = useCallback(() => {
-    console.log(`before closing: ${src}`)
+  const makeClientCrop = async (crop) => {
+    if (imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await getCroppedImg(
+        imageRef,
+        crop,
+      );
+      setNewImageUrl(croppedImageUrl);
+    }
+  }
+
+  const getCroppedImg = async (image, crop) => {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise<string>((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error('Canvas is empty');
+          return;
+        }
+        window.URL.revokeObjectURL(fileUrl);
+        fileUrl = window.URL.createObjectURL(blob);
+        resolve(fileUrl);
+      }, 'image/jpeg');
+    });   
+  }
+
+  const handleSave = () => {
+    console.log(newImageUrl);
+  }
+
+  const handleCancel = useCallback(() => {
     setSrc(null);
-    console.log(`after closing: ${src}`)
   }, [open]);
 
   return (
@@ -103,8 +149,8 @@ const ProfilePicture = ({ profilePageData }) => {
       <Dialog
         open={src !== null}
         fullScreen={isMobile}
-        onClose={handleClose}
-        fullWidth 
+        onClose={handleCancel}
+        scroll={'body'}
       >
         <DialogTitle>
           Update Profile Picture
@@ -119,11 +165,16 @@ const ProfilePicture = ({ profilePageData }) => {
               onImageLoaded={onImageLoaded}
               onComplete={onCropComplete}
               onChange={onCropChange}
-              style={{ maxWidth: "75%" }}
+              style={{ maxWidth: "100%" }}
               imageStyle={{ maxWidth: "100%" }}
             />
           )}
+          <img src={newImageUrl} />
         </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSave}>Save</Button>
+          <Button onClick={handleCancel}>Cancel</Button>
+        </DialogActions>
       </Dialog>
     </div>
   )
