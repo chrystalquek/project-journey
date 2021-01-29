8036ee298505d87f4cf4ecf75d4e9b4e1343087a
Akhil Vuputuri <e0309566@u.nus.edu>
2021-01-29 15:02:31 +0800

    Use ENV instead of NODE_ENV

--- server/src/loaders/connection.ts
+++ server/src/loaders/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 // Production uses one DB while staging and development share a DB
-const CONNECTION_STRING = process.env.NODE_ENV === 'production' ? 'mongodb+srv://user:user@cluster0.w711i.mongodb.net/user?retryWrites=true&w=majority' : 'mongodb+srv://user:user@cluster0.8ap9j.gcp.mongodb.net/user?retryWrites=true&w=majority';
+const CONNECTION_STRING = process.env.ENV === 'production' ? 'mongodb+srv://user:user@cluster0.w711i.mongodb.net/user?retryWrites=true&w=majority' : 'mongodb+srv://user:user@cluster0.8ap9j.gcp.mongodb.net/user?retryWrites=true&w=majority';
 mongoose.connect(CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
