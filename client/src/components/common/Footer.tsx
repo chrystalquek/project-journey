b1d33d58953636e598d2dbb49e6b9147da3503a2
DystoriaX <dascogabriel@gmail.com>
2021-02-14 23:49:16 +0800

    Fix footer issue

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,11 +1,24 @@
 import React, { FC } from 'react';
 import styles from '@styles/common/footer.styles';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
-const FooterComponent: FC = () => (
-  <footer style={styles.footer}>
-    <Typography variant="body2">Journey © 2021 Created by DSC NUS</Typography>
-  </footer>
-);
+const useStyles = makeStyles((theme) => ({
+  footerStyle: {
+    textAlign: 'center',
+    width: '100%',
+    marginTop: theme.spacing(4)
+  },
+}));
+
+const FooterComponent: FC = () => {
+  const classes = useStyles();
+  
+  return (
+    <footer className={classes.footerStyle}>
+      <Typography variant="body2">Journey © 2021 Created by DSC NUS</Typography>
+    </footer>
+  );
+}
 
 export default FooterComponent;
