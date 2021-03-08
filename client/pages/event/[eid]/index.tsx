169b47a0c2bf1e8867b346ab5a45020f1639e5ea
Matthew Alexander <alexandermatthew12@gmail.com>
2021-03-08 15:47:11 +0800

    Allow unregistered users to see event details

--- client/pages/event/[eid]/index.tsx
+++ client/pages/event/[eid]/index.tsx
@@ -2,11 +2,9 @@ import { useRouter } from 'next/router';
 import { Container } from '@material-ui/core';
 import React from 'react';
 import { EventDetails } from '@components/event/EventDetails';
-import { checkLoggedIn } from '@utils/helpers/auth';
 
 // Handles checking that user is logged in and event detail validity
 const EventsDetailPage = () => {
-  checkLoggedIn()
   const router = useRouter();
   const { eid } = router.query;
 
