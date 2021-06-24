6d888f65be16738e015fbf18088c689b7f5473ae
Zhang-Yunjie <e0421414@u.nus.edu>
2021-06-24 10:51:48 +0800

    Modify validation for update method of commitmentApplication

--- server/src/validations/commitmentApplication.ts
+++ server/src/validations/commitmentApplication.ts
@@ -20,18 +20,16 @@ export const getValidations = (method: CommitmentApplicationValidatorMethod): Va
         body('volunteerId', 'volunteer ID is not a object id').isMongoId(),// not sure if the ref to Volunteer needs to be checked as well
         body('status', 'status does not exist').exists(),
         body('status', 'status is not a string').isString(), // not sure if it's redundant as enum is checked on next line and all members of this enum are string
-        body('status', 'status is not a valid commitment application status').custom((value, { req }) => value in COMMITMENT_APPLICATION_STATUS), // tried to use isIn() but it's not for readonly[] COMMITMENT_APPLICATION_STATUS
+        body('status', 'status is not a valid commitment application status').custom((value: string, { req }: any) => value in COMMITMENT_APPLICATION_STATUS), // tried to use isIn() but it's for any[], not for readonly[] COMMITMENT_APPLICATION_STATUS
         body('createdAt', 'time of creation does not exist').exists(),
         body('createdAt', 'time of creation is of wrong date format').isISO8601(),
       ];
     }
     case 'updateCommitmentApplication': {
       return [
-        body('volunteerId', 'volunteer ID is not a object id').isMongoId(),// not sure if the ref to Volunteer needs to be checked as well
-        body('status', 'status does not exist').exists(),
-        body('status', 'status is not a string').isString(), // not sure if it's redundant as enum is checked on next line and all members of this enum are string
-        body('status', 'status is not a valid commitment application status').custom((value, { req }) => value in COMMITMENT_APPLICATION_STATUS), // tried to use isIn() but it's not for readonly[] COMMITMENT_APPLICATION_STATUS
-        body('createdAt', 'time of creation does not exist').exists(),
+        body('volunteerId', 'volunteer ID is not a object id').isMongoId(),
+        body('status', 'status is not a string').isString(), 
+        body('status', 'status is not a valid commitment application status').custom((value: string, { req }: any) => value in COMMITMENT_APPLICATION_STATUS),
         body('createdAt', 'time of creation is of wrong date format').isISO8601(),
       ];
     }
