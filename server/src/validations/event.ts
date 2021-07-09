4828908e263753f85d007e7bf14ad4894ef5b191
Zhang-Yunjie <e0421414@u.nus.edu>
2021-07-09 19:01:06 +0800

    Update event validation

--- server/src/validations/event.ts
+++ server/src/validations/event.ts
@@ -29,31 +29,36 @@ function isArrayOfRoleData(value: Array<any>): boolean {
   return value.every(item => isOfTypeRoleData(item));
 };
 
-
+// Define validation for each field 
+const name = body('name').exists().withMessage('Name is required').isString().withMessage('Name must be a string');
+const volunteerType = body('volunteerType').exists().withMessage('Volunteer type is required').custom((volunteerType: string) => stringEnumValidator(
+  VOLUNTEER_TYPE,
+  'Volunteer Type',
+  volunteerType,
+)).withMessage('Volunteer type is invalid');
+const eventType = body('eventType').exists().withMessage('Event type is required').custom((eventType: string) => stringEnumValidator(
+  EVENT_TYPE,
+  'Event Type',
+  eventType,
+)).withMessage('Event type is invalid');
+const startDate = body('startDate').exists().withMessage('Start date is required').custom((value, { req }) => value <= req.body.endDate)
+  .withMessage('Start date must be after end date')
+  .isISO8601()
+  .withMessage('Start date is of wrong date format');
+const endDate = body('endDate').exists().withMessage('End date is required')
+  .isISO8601()
+  .withMessage('End date is of wrong date format');
 const getValidations = (method: EventValidatorMethod) => {
   switch (method) {
     case 'createEvent': {
       return [
-        body('name', 'name does not exist').exists(),
-        body('name', 'name is not a string').isString(),
+        name,
         body('coverImage', 'cover image is not represented by a string').optional({ checkFalsy: true }).isString(),
-        body('eventType', 'eventType does not exist').exists(),
-        body('eventType', 'eventType is not valid').custom((eventType: string) => stringEnumValidator(
-          EVENT_TYPE,
-          'Event Type',
-          eventType,
-        )),
-        body('volunteerType', 'volunteerType does not exist').exists(),
-        body('volunteerType', 'volunteerType is not valid').custom((volunteerType: string) => stringEnumValidator(
-          VOLUNTEER_TYPE,
-          'Volunteer Type',
-          volunteerType,
-        )),
-        body('startDate', 'start date does not exist').exists(),
-        body('startDate', 'start date is after end date').custom((value, { req }) => value <= req.body.endDate),
-        body('startDate', 'start date is of wrong date format').isISO8601(),
-        body('endDate', 'end date does not exist').exists(),
-        body('endDate', 'end date is of wrong date format').isISO8601(),
+        eventType,
+        // Enum validator
+        volunteerType,
+        startDate,
+        endDate,
         body('deadline', 'deadline does not exist').exists(),
         body('deadline', 'deadline is of wrong date format').isISO8601(),
         body('vacancies', 'vacancy does not exist').exists(),
