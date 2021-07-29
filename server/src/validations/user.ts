c780c9773f5f3e8885011a5f4b71f0962cf34d7c
Zhang Yunjie <e0421414@u.nus.edu>
2021-07-30 03:16:27 +0800

    Update user validation

--- server/src/validations/user.ts
+++ server/src/validations/user.ts
@@ -3,19 +3,13 @@ import { existingEmailValidator, passwordValidator } from "./global";
 
 export type UserValidatorMethod = "login" | "updatePassword";
 
-const LENGTH_MINIMUM_PASSWORD = 8;
-
-const newPasswordValidator = body("newPassword").isString().isLength({
-  min: LENGTH_MINIMUM_PASSWORD,
-});
-
 const getValidations = (method: UserValidatorMethod) => {
   switch (method) {
     case "login": {
-      return [existingEmailValidator, passwordValidator];
+      return [existingEmailValidator, body("password", "Password must be a string").isString()];
     }
     case "updatePassword": {
-      return [existingEmailValidator, passwordValidator, newPasswordValidator];
+      return [existingEmailValidator, passwordValidator];
     }
     default:
       return [];
